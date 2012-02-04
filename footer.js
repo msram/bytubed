9feedfe6bb3fe7ee962c379014361e0cd9e4b6fc
M S Ram <msram1078@yahoo.com>
2012-02-05 05:08:37 +0530

    Modularizing the pages of bytubed@github

---
+++ footer.js
@@ -0,0 +1,24 @@
+document.write('<br/><hr/>');
+
+/* IntenseDebate Code */
+var idcomments_acct = "4aca98f14705a845798ff5146c6bb67d";
+var idcomments_post_id;
+var idcomments_post_url;
+document.write('<span id="IDCommentsPostTitle" style="display:none"></span>' +
+            '<scri' + 'pt type="text/javascript" src="http://www.intensedebate.com/js/genericCommentWrapperV2.js"></scr' + 'ipt>' + 
+            '<scri' + 'pt type="text/javascript" src="http://www.intensedebate.com/js/genericLinkWrapperV2.js"></scri' + 'pt>');
+/* End of IntenseDebate Code */
+
+
+/* Google Analytics Code */        
+var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
+document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
+
+document.write('<scri' + 'pt src="text/javascript"> var pageTracker = _gat._getTracker("UA-20281055-1");' +
+                'pageTracker._initData();' +
+                'pageTracker._trackPageview();</scr' + 'ipt>'
+            );
+/* End of Google Analytics Code */
+
+document.write('<span class="rfloat italic info">Last Modified:' + document.lastModified + '</span><br/><br/>');
+
---
+++ header.js
@@ -0,0 +1,27 @@
+document.write
+    (
+        '<a href="http://github.com/msram"><img style="position: absolute; top: 0; right: 0; border: 0;" src="https://a248.e.akamai.net/assets.github.com/img/71eeaab9d563c2b3c590319b398dd35683265e85/687474703a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f72696768745f677261795f3664366436642e706e67" alt="Fork me on GitHub"></a>' +
+        '<div class="download">' + 
+            '<a href="https://github.com/msram/bytubed/zipball/master"><img border="0" width="90" src="https://github.com/images/modules/download/zip.png"></a>' +
+            '<a href="https://github.com/msram/bytubed/tarball/master"><img border="0" width="90" src="https://github.com/images/modules/download/tar.png"></a>' +
+        '</div>' +
+        '<h1>' +
+            '<a href="https://github.com/msram/bytubed">BYTubeD</a> ' +
+            '<span class="small">by <a href="https://github.com/msram">msram</a></span>' +
+        '</h1>' +
+
+        '<div class="description">' +
+            '(A <a href="https://addons.mozilla.org/addon/bytubed/">Firefox extension</a> to download YouTube videos in bulk)' +
+        '</div>'
+    );
+            
+document.write
+    (
+        '<hr/>' +
+        '<a href="index.html">Home</a>&nbsp;&nbsp;&nbsp;&nbsp; | &nbsp;&nbsp;&nbsp;&nbsp;' +
+        '<a href="help.html">Help</a> ' +
+        '<br/><hr/><br/>' 
+    );
+    
+
+        
