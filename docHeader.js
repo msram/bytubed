480fe0a572fb36643e43d0181070dbd4043b6447
M S Ram <msram1078@yahoo.com>
2012-02-05 17:13:46 +0530

    style changes to home page

---
+++ docHeader.js
@@ -0,0 +1,6 @@
+document.write(   
+        '<meta charset="utf-8">' +
+        '<link rel="stylesheet" type="text/css" href="style.css" />'
+    );
+if(navigator.userAgent.indexOf("Trident") != -1)
+    window.location.href = "ie.html";
--- header.js
+++ header.js
@@ -1,9 +1,10 @@
 document.write
     (
-        '<a href="http://github.com/msram"><img style="position: absolute; top: 0; right: 0; border: 0;" src="https://a248.e.akamai.net/assets.github.com/img/71eeaab9d563c2b3c590319b398dd35683265e85/687474703a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f72696768745f677261795f3664366436642e706e67" alt="Fork me on GitHub"></a>' +
+        
+        '<a href="http://github.com/msram"><img style="position: absolute; top: 0; right: 0; border: 0;" src="images/fork.png" alt="Fork me on GitHub"></a>' +
         '<div class="download">' + 
-            '<a href="https://github.com/msram/bytubed/zipball/master"><img border="0" width="90" src="https://github.com/images/modules/download/zip.png"></a>' +
-            '<a href="https://github.com/msram/bytubed/tarball/master"><img border="0" width="90" src="https://github.com/images/modules/download/tar.png"></a>' +
+            '<a href="https://github.com/msram/bytubed/zipball/master"><img border="0" width="90" src="images/zip.png"></a>' +
+            '<a href="https://github.com/msram/bytubed/tarball/master"><img border="0" width="90" src="images/tar.png"></a>' +
             '<div class="info">Source code of BYTubeD<br/>(from GitHub)</div>' +
         '</div>' +
         '<h1 class="header">' +
@@ -11,6 +12,7 @@ document.write
         '</h1>' +
         '<div class="description">' +
             'A <a href="https://addons.mozilla.org/addon/bytubed/">Firefox extension</a> by <a href="https://github.com/msram">msram</a> to download YouTube videos in bulk' +
+            '' +
         '</div>'        
     );
 
