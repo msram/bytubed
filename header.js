c6f1d6986c14310a0c987e43e1c90f0b688e435a
M S Ram <msram1078@yahoo.com>
2012-02-06 10:40:01 +0530

    adding development.html, removed the zip icon near title

--- header.js
+++ header.js
@@ -3,10 +3,10 @@ document.write
         
         '<a href="http://github.com/msram"><img style="position: absolute; top: 0; right: 0; border: 0;" ' +
         'src="images/fork_me_gray.png" alt="Fork me on GitHub"></a>' +
+        
         '<div class="download">' + 
-            '<a href="https://github.com/msram/bytubed/zipball/master"><img border="0" width="90" src="images/zip.png"></a>' +
             '<a href="https://github.com/msram/bytubed/tarball/master"><img border="0" width="90" src="images/tar.png"></a>' +
-            '<div class="info">Source code of BYTubeD<br/>(from GitHub)</div>' +
+            '<div class="info">Source code<br/>(from GitHub)</div>' +
         '</div>' +
         '<h1 class="header">' +
             '<a href="https://github.com/msram/bytubed">BYTubeD - Bulk (Batch) YouTube video Downloader</a> ' +
@@ -23,7 +23,8 @@ document.write
         '<hr class="header"/>' +
         '<a href="index.html">Home</a>&nbsp;&nbsp;&nbsp;&nbsp; | &nbsp;&nbsp;&nbsp;&nbsp;' +
         '<a href="help.html">Help</a>&nbsp;&nbsp;&nbsp;&nbsp; | &nbsp;&nbsp;&nbsp;&nbsp;' +
-        '<a href="screen-shots.html">Screenshots</a>' +
+        '<a href="screen-shots.html">Screenshots</a>&nbsp;&nbsp;&nbsp;&nbsp; | &nbsp;&nbsp;&nbsp;&nbsp;' +
+        '<a href="development.html">Development</a>' +
         '<br/><hr class="header"/>' 
     );
 /* End of Navigation Bar */
