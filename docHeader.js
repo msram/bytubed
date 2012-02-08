49c5e569873551b6fa27768658a83520eed299d8
M S Ram <M.S.Ramaiah@gmail.com>
2012-02-08 21:42:35 +0530

    Everything has been made jekyll friendly; Replacing IntenseDebate comments by Disqus comments.

--- docHeader.js
+++
@@ -1,7 +0,0 @@
-document.write(   
-        '<meta charset="utf-8">' +
-        '<link rel="stylesheet" type="text/css" href="style.css" />'
-    );
-
-if(navigator.userAgent.indexOf("Trident") != -1)	// IE is not a supported browser
-    window.location.href = "ie.html";
