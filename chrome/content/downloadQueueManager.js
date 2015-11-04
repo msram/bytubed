aba873eed21f0364791da45e0fc06843ebfa1f2e
dieterds <bytubed2@gmail.com>
2015-11-04 10:43:06 +0100

    v1.2.3
    - removed unneeded code

--- chrome/content/downloadQueueManager.js
+++ chrome/content/downloadQueueManager.js
@@ -80,66 +80,6 @@ if (Downloads.getList) {  // use Downloads.jsm
         });
     }
 }
-else {  // Old
-    var ioService = Cc["@mozilla.org/network/io-service;1"].getService(Ci.nsIIOService),
-        dm = Cc["@mozilla.org/download-manager;1"].createInstance(Ci.nsIDownloadManager);
-    get = function (callback, pointer) {
-        var dl,
-            persist = Cc["@mozilla.org/embedding/browser/nsWebBrowserPersist;1"].createInstance(Ci.nsIWebBrowserPersist);
-        persist.persistFlags = persist.PERSIST_FLAGS_REPLACE_EXISTING_FILES | persist.PERSIST_FLAGS_AUTODETECT_APPLY_CONVERSION;
-
-        var listener;
-        var mListener = function (download) {
-            this.download = download;
-        }
-        mListener.prototype = {
-            download: null,
-            onStatusChange: function (aWebProgress, aRequest, aStatus, aMessage, aDownload) { },
-            onSecurityChange: function (prog, req, state, dl) { },
-            onProgressChange: function (prog, req, prog, progMax, tProg, tProgMax, dl) {
-                if (dl.id != this.download.id) return;
-                if (callback && callback.progress) callback.progress.apply(pointer, [dl]);
-            },
-            onStateChange: function (prog, req, flags, status, dl) { },
-            onDownloadStateChange: function (state, dl) {
-                if (dl.id != this.download.id) return;
-
-                if (dl.state == Ci.nsIDownloadManager.DOWNLOAD_FINISHED) {
-                    dm.removeListener(this);
-                    if (callback && callback.done) callback.done.apply(pointer, [dl]);
-                }
-                else if (dl.state == Ci.nsIDownloadManager.DOWNLOAD_PAUSED) {
-                    if (callback && callback.paused) callback.paused.apply(pointer, [dl]);
-                }
-                else if (dl.state == Ci.nsIDownloadManager.DOWNLOAD_FAILED ||
-                    dl.state == Ci.nsIDownloadManager.DOWNLOAD_CANCELED ||
-                    dl.state == Ci.nsIDownloadManager.DOWNLOAD_BLOCKED_PARENTAL ||
-                    dl.state == Ci.nsIDownloadManager.DOWNLOAD_BLOCKED_POLICY ||
-                    dl.state == Ci.nsIDownloadManager.DOWNLOAD_DIRTY) {
-                    dm.removeListener(this);
-                    if (callback && callback.error) callback.error.apply(pointer, [dl]);
-                }
-            }
-        }
-
-        return function (url, file, aPrivacyContext, aIsPrivate, callback2, pointer2) {
-            // Create URI
-            var urlURI = ioService.newURI(url, null, null),
-                fileURI = ioService.newFileURI(file);
-            // Start download, Currently the extension is not available on private mode due to panel module incompatibility
-            dl = dm.addDownload(dm.DOWNLOAD_TYPE_DOWNLOAD, urlURI, fileURI, null, null, null, null, persist, aIsPrivate || false);
-            listener = new mListener(dl);
-            dm.addListener(listener);
-            persist.progressListener = dl.QueryInterface(Ci.nsIWebProgressListener);
-            persist.saveURI(dl.source, null, null, null, null, file, aPrivacyContext);
-
-            if (callback2) callback2.apply(pointer2, [dl]);
-        }
-    };
-    cancel = function (id) {
-        dm.cancelDownload(id);
-    };
-}
 
 iitk.cse.cs213.bytubed.DownloadQueueManager = function(callBack, errorHandler, destDir, vList, 
                                                         prefs, subtitleLanguageInfo)
